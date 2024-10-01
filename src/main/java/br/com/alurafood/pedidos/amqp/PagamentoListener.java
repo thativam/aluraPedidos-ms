c26a4a81ca5c02786b23e0a17b87451ab3714c1c
thativam <thativancapeli@outlook.com>
2024-10-01 18:29:22 -0300

    feat: integracao com rabbit mq realizado e configurado.

---
+++ src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -0,0 +1,26 @@
+package br.com.alurafood.pedidos.amqp;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import br.com.alurafood.pedidos.dto.PagamentoDto;
+
+@Component
+public class PagamentoListener {
+
+    @RabbitListener(queues = "pagamento.concluido")
+    public void recebeMensagem(PagamentoDto pagamento) {
+        String mensagem = """
+                Dados do pagamento: %s
+                Número do pedido: %s
+                Valor R$: %s
+                Status: %s 
+                Nome: %s
+                """.formatted(pagamento.getId(),
+                pagamento.getPedidoId(),
+                pagamento.getValor(),
+                pagamento.getStatus(),pagamento.getNome());
+        System.out.println(mensagem);
+    }
+}
+
